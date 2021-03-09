072c4f5a54cf5dfa45d77bd496852874417bf5c7
ttronge <tomastronge@gmail.com>
2021-03-09 16:59:13 -0300

    estructura de rutas

---
+++ arwines/src/store/store.js
@@ -0,0 +1,13 @@
+// esto es el corazon de nuestro store
+// van a estar todos los estados 
+import { configureStore } from '@reduxjs/toolkit'
+import logger from 'redux-logger'
+import userReducer from './user'
+
+const store = configureStore({
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware().concat(logger),
+    reducer: { // aca va todo nuestro estados 
+        user: userReducer // es user reducer van a ir todas las cosas que van a ir dentro de user
+    }
+})
+export default store
